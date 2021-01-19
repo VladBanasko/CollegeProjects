4e370e3c5faa8b6843eac8d8741f4511f4cb9cf8
Vladyslav Banasko <vlad.bona@gmail.com>
2021-01-18 22:35:05 -0500

    Update server.py
    update server status info

--- messenger/server.py
+++ messenger/server.py
@@ -1,12 +1,23 @@
 import flask
+import time
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello, World!"
 
+
+@app.route("/status")
+def status():
+    return {
+        'status': True,
+        'name': 'Python messenger',
+        'time': time.time(),
+        'time':time.asctime(),
+    }
 
 app.run()
