0094cf01b193c9555950bdb73d86c9ebbf293b55
Vladyslav Banasko <vlad.bona@gmail.com>
2021-01-25 21:17:59 -0500

    receiver file added
    receiver method added

--- messenger/sender.py
+++ messenger/sender.py
@@ -1,4 +1,9 @@
 import requests
 
-response = requests.post('http://127.0.0.1:5000/send',
-                          json={'name': 'Nick', 'text': '123'})
+name = input('Input name: ')
+
+
+while True:
+    text = input('Inpute text: ')
+    response = requests.post('http://127.0.0.1:5000/send',
+                          json={'name': name, 'text': text})
